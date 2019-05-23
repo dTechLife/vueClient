952771acbc1965c9a3ee1b07ac004f7100009a27
DestinTech <destintech@gmail.com>
2019-05-23 16:06:16 -0600

    added axios and promises for API calls to backend

---
+++ src/components/List.js
@@ -0,0 +1,4 @@
+Vue.component('List',{
+    template: "<p> Hello! </p>"
+
+})
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 //import list from './components/List.js'
 // import listAdd from './components/listAdd.js'
 // import navBar from './components/navBar.js'
+let apiURL = "http://localhost:3000"
+
+
 
 
 var app = new Vue({
@@ -8,20 +11,36 @@ var app = new Vue({
     data:{
           groccerys:[
                 //we want to fetch this object from a database
-            {id:"1", name:"bannanas", quantity:"Quantity", unit:"grams", done:false}
-      
+            {
+                  id:"1", name:"bannanas", quantity:"Quantity", unit:"grams", done:false
+            }
+            
       ]},
 
       methods:{
             addTodo(event){     
+                
                   const name = event.target.value //sets the const name to value from the event that fired the function
                   this.groccerys.push({name, done:false, id:Date.now()})
+                  axios
+                  .post(apiURL+'/api/groccerylists',{
+                        name:name,
+                        ingredients:{"potatoes":1},
+                        yield:0,
+                        yieldUnit:"lbs",
+                        servingCalories:100})
+                  .then (function(response){
+                        console.log(response);})
+                  .catch(function(error){
+                        console.log(error);
+                  })
                   event.target.value = ''
             },
 
             removeTodo(id){
-                  this.groccerys = this.groccerys.filter(function(groccerys){ return groccerys.id !== id})  // this is a list function, it's made to run on lists.    
-              
+                  this.groccerys = this.groccerys.filter(function(groccerys){return groccerys.id !== id})  // this is a list function, it's made to run on lists.    
+                  axios 
+                      .delete(apiURL) 
             },
             
             check(groccery){
@@ -31,7 +50,28 @@ var app = new Vue({
       },
       mounted(){
             axios
-                  .get('url')
-                  .then(function(response){ this.groccerys = response})
+                  .get(apiURL+'/api/groccerylists')
+                  .then(function(response){ app.groccerys = response.data, console.log(response)})
+                  
       }
 })
+
+      // [
+      // {
+      //       "name": "Baked potatoes",
+      //       "ingredients": {
+      //         "potatoes": {
+      //           "amount": "3",
+      //           "unit": "potato"
+      //         },
+      //         "butter": {
+      //           "amount": 5,
+      //           "unit": "tsp"
+      //         }
+      //       },
+      //       "yield": 3,
+      //       "yieldUnit": "baked potatoes",
+      //       "servingCalories": 300,
+      //       "id": 1
+      //     }
+      //   ]
